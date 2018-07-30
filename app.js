3d5e3848762231b32c74a81fb166906dca461c3e
Andrew Maul <andrew.maul@meltmedia.com>
2018-07-30 15:31:47 -0700

    Basic working prototype

--- app.js
+++ app.js
@@ -1,62 +1,79 @@
-var fs = require('fs');
+var fse = require('fs-extra');
 var jimp = require('jimp');
 var rp = require('request-promise');
 
-
+var cardArray;
 
 readInputDirectory();
 
+function readInputDirectory() {
+    fse.readFile('./input/list.txt', 'utf8')
+        .then((data) => {
+            console.log("Reading list from local directory.");
+            inputToArray(data);
+            initJimp();
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
 
 function inputToArray(input) {
   var list = String(input);
   list =  list.replace(/1x |1x/g,'');
   list = list.replace(/ /g,'+');
-  return list.split('\n');
+  cardArray = list.split('\n');
 }
 
-function readInputDirectory() {
-    fs.readFile('./input/list.txt', 'utf8', (err, data) => {
-        if (err) throw err;
-        console.log(inputToArray(data));
-        requestImagesFromEndpoint(inputToArray(data));
-    });
+function initJimp() {
+    console.log("Initializing JIMP.");
+    var blankCanvas = new jimp(2048,842,0xFFFFFFFF).opacity(0);
+    compositeImages(blankCanvas);
+}
+
+function compositeImages(startingImage) {
+    console.log("Compositing images.");
+
+    requestImageData("https://api.scryfall.com/cards/named?exact="+cardArray[0]+"&format=image")
+        .then((response) => {
+            var retrievedCardImage = new jimp(response, function(){});
+            startingImage.composite(retrievedCardImage, getRandomInt(10)*100,getRandomInt(10)*100);
+            cardArray.splice(0,1);
+            if(cardArray.length) {
+                compositeImages(startingImage);
+            }
+            else {
+                finalizeImage(startingImage);
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
 }
 
 function requestImageData(url) {
-rp(url)
-    .then(function (data) {
-        sendImageToJimp(JSON.parse(data));
-        // console.log(JSON.parse(data));
-    })
-    .catch(function (err) {
-        // Crawling failed...
-    });
+    var options = {
+      uri: url,
+      method: 'GET',
+      encoding: null
+    };
+    return rp(options)
+        .then((data) => {
+          console.log("Image request successful.");
+          return data;
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 
 }
 
-function sendImageToJimp(cardJSON) {
-    jimp.read(cardJSON.image_uris.large, function (err, image) {
-        if (err) throw err;
-        image.resize(256, 256)
-             .quality(60)
-             .greyscale()
-             .write("./output/test.jpg");
-    });
+function finalizeImage(finalImage) {
+    console.log("Done compositing. Writing to output.");
+    finalImage.write("./output/final.png");
 }
-
-
-function requestImagesFromEndpoint(arrayOfCardNames) {
-    for (var i = arrayOfCardNames.length - 1; i >= 0; i--) {
-        requestImageData("https://api.scryfall.com/cards/named\?exact\="+arrayOfCardNames[i]);
-    }
-}
-
-
-
-
-
-
-
-
-
-
